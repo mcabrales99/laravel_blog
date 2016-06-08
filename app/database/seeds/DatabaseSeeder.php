1808a564b10958de330686ee8beaa8eb2b5dcd96
Mauro Cabrales <maurocabrales@Macs-MacBook-Pro-2.local>
2016-06-08 09:37:58 -0500

    This file holds a seeder for the database

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -10,8 +10,13 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
+		DB::table('posts')->delete();
+		DB::table('users')->delete();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
+		$this->call('PostTableSeeder');
+
+		
 	}
 
 }
