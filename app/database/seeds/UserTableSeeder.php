2e7d00a14dc23319a0a492f5df8d9337cc1b1fe2
Mauro Cabrales <maurocabrales@Macs-MacBook-Pro-2.local>
2016-06-08 14:51:54 -0500

    user table modified

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -6,27 +6,27 @@ class UserTableSeeder extends Seeder {
     {
         DB::table('users')->delete();
 
-       $user =new user();
+       $user =new User();
        $user->email  = 'mcabrales99@gmail.com';
        $user->password = $_ENV['USER_PASSWORD'];
        $user->save();
 
-       $user =new user();
+       $user =new User();
        $user->email  = 'yipeee@yahoo.com';
        $user->password = $_ENV['USER_PASSWORD'];
        $user->save();
 
-       $user =new user();
+       $user =new User();
        $user->email  = 'blah@yahoo.com';
        $user->password = $_ENV['USER_PASSWORD'];
        $user->save();
 
-       $user =new user();
+       $user =new User();
        $user->email  = 'weight@yahoo.com';
        $user->password = $_ENV['USER_PASSWORD'];
        $user->save();
 
-       $user =new user();
+       $user =new User();
        $user->email  = 'whoknows@yahoo.com';
        $user->password = $_ENV['USER_PASSWORD'];
        $user->save();
